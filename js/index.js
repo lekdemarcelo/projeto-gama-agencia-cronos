21ff5a4006ae9fee5446acdd694b07fee94ea0a5
samuellgc <samuellgc@hotmail.com>
2021-12-27 15:04:21 -0300

    Redução do código no arquivo index.js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,17 @@
 const btnAdd = document.querySelector('.btn-add');
 const abrirModal = document.querySelector('.fundo-modal');
-let btnFecharModal = document.querySelector('.btn-cancel');
-let btnSalvar = document.querySelector('.btn-save');
+const btnFecharModal = document.querySelector('.btn-cancel');
+const btnSalvar = document.querySelector('.btn-save');
 
 btnAdd.addEventListener('click', (e) => {
     e.preventDefault();
     abrirModal.classList.remove('invisible')
 })
 
-function fecharModal() {
-    btnFecharModal.addEventListener('click', (e) => {
-        e.preventDefault();
-        abrirModal.classList.add('invisible');
-        limparInputsCriacao();
+btnFecharModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    abrirModal.classList.add('invisible');
+    limparInputsCriacao();
     })
-};
 
-fecharModal();
-
-const limparInputsCriacao = () => {
-    document.querySelector('.form-modal').reset();
-}
+const limparInputsCriacao = () => document.querySelector('.form-modal').reset();    
