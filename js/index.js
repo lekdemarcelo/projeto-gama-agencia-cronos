b3b5f5309632ca6afb0e7363b6f8922d1ae247fb
samuellgc <samuellgc@hotmail.com>
2022-01-05 15:00:50 -0300

    Inclusão da função excluir, ajustes na função add

--- js/index.js
+++ js/index.js
@@ -1,64 +1,30 @@
-const btnAdd = document.querySelector('.btn-add');
-const abrirModal = document.querySelector('.fundo-modal');
-const btnFecharModal = document.querySelector('.btn-cancel');
-const btnSalvar = document.querySelector('.btn-save');
-
-
-
-btnAdd.addEventListener('click', (e) => {
-    e.preventDefault();
-    abrirModal.classList.remove('invisible')
-})
-
-btnFecharModal.addEventListener('click', (e) => {
-    e.preventDefault();
-    abrirModal.classList.add('invisible');
-    limparInputsCriacao();
-    })
-
-const limparInputsCriacao = () => document.querySelector('.form-modal').reset();    
-
 //Função que ativa ao enviar o formulário adicionando um serviço novo
-function adicionarServico(){
-    let nome = document.getElementById('servico').value;
-    let arquivo = document.getElementById('arquivo').value;
-    let descricao = document.getElementById('descricao').value;
-    let tabela = document.getElementById('minha-tabela');
+function adicionarServico() {
+  let nome = document.getElementById('servico').value;
+  let arquivo = document.getElementById('arquivo').value;
+  let descricao = document.getElementById('descricao').value;
 
-    //Preview da imagem no formulario
-var preview = document.getElementById('previewimage');
-preview.style.display= "none";
-var carregarImagem = function(event) {
-  preview.style.display="block"
-  preview.src= arquivo.value
-};
- //Condicional para impedir de criar um serviço em branco
- if(nome.length>0 && descricao.length>2 && arquivo.length>5){
-
+  // Condicional para impedir de criar um serviço em branco
+  if (nome.length > 0 && descricao.length > 2 && arquivo.length > 5) {
+    
     //Criação do serviço na tabela
-    let tamanhoTabela = tabela.rows.length; //Calculando o tamanho da Tabela
-    let linha = tabela.insertRow(tamanhoTabela); //Inserindo uma linha abaixo da Tabela
-    let cell1 = linha.insertCell(0); //Inserindo as celulas na linha nova
-    let cell2 = linha.insertCell(1);
-    let cell3 = linha.insertCell(2);
-    let cell4 = linha.insertCell(3);
-
-    //Criação do Botão de editar e remover para inserir na célula
-    let btnEditar = '<button class="btn btn-secondary m-1">editar</button>'
-    let btnRemover = '<button class="btn btn-danger m-1">excluir</button>'
-
-    //Aqui estamos fazendo a inserção dos dados nas celulas novas da tabela
-    cell1.innerHTML = nome
-    cell2.innerHTML = '<img src="' + arquivo + '" class="img-thumbnail h-50"/>'
-    cell3.innerHTML = descricao
-    cell4.innerHTML = btnEditar + btnRemover 
+    const newCourse = document.createElement('tr')
+    newCourse.innerHTML = `
+    <td>${nome}</td>
+    <td> <img class="img-course img-fluid table-img" src="${arquivo}" alt=""></td>
+    <td class="descrip-course">${descricao}</td>
+    <td class="btn-action"><button class="btn btn-secondary btn-edit">EDIT</button> <button class="btn btn-danger btn-delete" onclick="excluir(${nome})">DELETE</button></td>
+    `
+    newCourse.setAttribute('id', `${nome}`);
+    document.getElementById('container-body').appendChild(newCourse);
 
     //Nesta etapa forneço um valor em branco para apagar todos os campos ao enviar
-    document.getElementById('servico').value = "";
-    document.getElementById('arquivo').value = "";
-    document.getElementById('descricao').value = "";
+    document.getElementById('modalform').reset()
+  } else {
+    window.alert('Estão faltando dados para inserir o serviço novo!')
+  }
+}
 
-
-} else{
-  window.alert('Estão faltando dados para inserir o serviço novo!')
-}}
+const excluir = (id) => { 
+  document.getElementById('container-body').removeChild(id);
+}
